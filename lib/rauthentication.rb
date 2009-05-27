1049e0c4725b50e5137c9b91f1249412f4a09b98
Thomas Boerger <t.boerger@mosez.net>
2009-05-27 15:31:51 +0200

    fixed typo on rauthentication include

--- lib/rauthentication.rb
+++ lib/rauthentication.rb
@@ -1,4 +1,4 @@
 require File.dirname(__FILE__) + "/rauthentication/base"
 
 ActionController::Base.send( :include, RAuthentication::Base )
-ActionView::Base.send( :include, RAuthoentication::Base::ControllerInstanceMethods )
+ActionView::Base.send( :include, RAuthentication::Base::ControllerInstanceMethods )
